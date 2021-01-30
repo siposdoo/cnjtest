294f04de4c5e890220b74059f715cf8ae29bc010
Stanislav Despotović <stanislav@mvpfactory.co>
2021-01-31 00:17:29 +0100

    Added basic template look for future app

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,23 +1,74 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
+  <div>
+    <b-container class="mt-3">
+      <b-row>
+        <b-col>
+          <b-row>
+            <b-col>
+              <b-form-file
+                v-model="file1"
+                :state="Boolean(file1)"
+                placeholder="Choose a CSV file for proccessing or drop it here..."
+                drop-placeholder="Drop file here..."
+              >
+              </b-form-file>
+            </b-col>
+          </b-row>
+          <b-row v-if="file1" align-v="end">
+            <b-col>
+              <div>
+                <i>Selected file: {{ file1 ? file1.name : "" }}</i>
+              </div>
+            </b-col>
+          </b-row>
+          <b-row v-if="file1" align-v="end">
+            <b-col>
+              <b-form-checkbox
+                value="1"
+                unchecked-value="0"
+                v-model="saveToMysql"
+              >
+                Save to MySql
+              </b-form-checkbox>
+            </b-col>
 
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+            <b-col class="d-flex flex-row-reverse bd-highlight">
+              <button class="mt-3 btn btn-primary" v-on:click="submitFile()">
+                Submit & parse CSV
+              </button>
+            </b-col>
+          </b-row>
+        </b-col>
+        <b-col>
+          <b-row>
+            <ul class="list-group w-100">
+              <li class="list-group-item active">MySql data info</li>
+              <li class="list-group-item">
+                Last inserted ID: {{ lastInsertedId }}
+              </li>
+              <li class="list-group-item">Row counts: {{ rowCounts }}</li>
+            </ul>
+          </b-row>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
-    }
+export default {
+  mounted() {},
+  data() {
+    return {
+      file1: null,
+      lastInsertedId: null,
+      rowCounts: null,
+    };
+  },
+  methods: {
+      submitFile(){
+          
+      }
+  },
+};
 </script>
